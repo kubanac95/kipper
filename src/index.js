7d413475f352594171082e98f8e721b4cd5da11c
kubanac95 <aleksandar.nikolic.01@outlook.com>
2020-05-06 22:04:33 +0200

    Adding packages.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { Text } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>Hello world!</Text>
-    </View>
+    </NavigationContainer>
   );
 };
 
