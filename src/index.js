a28dc4da0542917b500a76faa379ea6a5b02b49f
kubanac95 <aleksandar.nikolic.01@outlook.com>
2020-05-06 23:44:26 +0200

    Dummy google sign in

--- src/index.js
+++ src/index.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-community/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '119018618906-bh3kje0dflkhnafgo3889mab8t9ed1oe.apps.googleusercontent.com',
+  scopes: ['profile', 'email'],
+});
+
 const App = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+
+        <Button
+          title="Google Sign-In"
+          onPress={() =>
+            onGoogleButtonPress().then(console.log).catch(console.log)
+          }
+        />
+      </View>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <Text>Hello world!</Text>
-    </NavigationContainer>
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
   );
 };
 
